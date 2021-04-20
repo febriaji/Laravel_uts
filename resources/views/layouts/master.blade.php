a280bd5c17a7effeb4abe55805b44963930c331f
febriaji <rizqiaminudinf@gmail.com>
2021-04-20 22:08:03 +0700

    make it better

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-        <a class="navbar-brand" href="#">Pemkot Tegal</a>
+        <a class="navbar-brand" href="#">Pemerintah Desa Dumeling</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -31,6 +31,7 @@
         <div class="container">
             @yield('content')
         </div>
+        
 
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -38,4 +39,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
     </body>
+    <footer class="bg-light text-center text-lg-start" style="padding-bottom: 0px;
+    position:fixed;
+    bottom:0;">
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2020 Copyright:
+                <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
 </html>
